555bfb1c377f9ac3aa8268a5c1f1b79f8aaf6fb0
Lazar <lazar@pixupplay.com>
2025-03-18 20:15:04 +0100

    Removed unnessesary tsconfigs, and added global.

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transformIgnorePatterns: ["/node_modules/"],
+};
