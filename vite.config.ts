102299a0d73b10d08630ec13ba29622b4a5775b0
Lazar <lazar@pixupplay.com>
2025-03-19 08:31:52 +0100

    changed configuration tsconfig node module resultion to node.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss';
-import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [react()],
 })
